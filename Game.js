f341d264279a3ea62f866544ba8d7f8b24179eb9
allVentures <paul@allphotobangkok.com>
2018-10-10 02:23:19 +0200

    half way through T140

--- Game.js
+++ Game.js
@@ -1,13 +1,54 @@
-var colors = [
-"RGB(255,0,0)",
-"RGB(255,0,240",
-"RGB(255,79,0)",
-"RGB(255,0,100)",
-"RGB(255,0,0)",
-"RGB(255,0,0)"
-];
+var colors = generateRandomColors(6);
+var squares = document.getElementsByClassName("square");
+var pickedColor = pickColor(); 				// random color from the color array 
+// console.log("picked color var: ",pickedColor);
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor;
+var messageDisplay = document.getElementById("message");
+var h1=document.getElementsByTagName("h1");
 
-var squares=document.getElementsByClassName("square");
-for(var i = 0; i < squares.length; i++){
-	squares[i].style.background = colors[i];
+for (var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+    squares[i].addEventListener("click", funcCompareColors);
+}
+
+function funcCompareColors() {
+    var clickedColor = this.style.background;
+    // console.log('clicked color',clickedColor);
+    if (clickedColor == pickedColor) {
+        messageDisplay.textContent = "Correct! ;-)";
+        messageDisplay.style.color = "green";
+        changeColors(clickedColor);
+        h1[0].style.background=clickedColor;
+    } else {
+        this.style.background = "#333333";
+        messageDisplay.textContent = "Try again!"
+    }
+}
+
+function changeColors(color) {
+    for (var i = 0; i < colors.length; i++) {
+        squares[i].style.background = color;
+    }
+}
+function pickColor(){
+	var random=Math.floor(Math.random()*colors.length);
+	// console.log('picked color:',random);
+	return colors[random];
+}
+
+function generateRandomColors(numColors) {
+	var colorArray=[];
+for (var i = 0; i < numColors; i++) {
+	colorArray.push(randomColor());
+	}
+    // console.log('color array',colorArray);
+	return colorArray;
+}
+
+function randomColor(){
+	var r=Math.floor(Math.random()*256);
+	var g=Math.floor(Math.random()*256);
+	var b=Math.floor(Math.random()*256);
+	return "rgb("+r+", "+g+", "+b+")";
 }
